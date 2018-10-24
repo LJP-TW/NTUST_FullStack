5a748253b677d73cabf2f39bbf4c905f1bbc02ee
LJP-TW <accr94238@gmail.com>
2018-10-24 13:06:02 +0800

    Jquery OK

--- src/app/t-to-b-slider-area/t-to-b-slider-area.component.ts
+++ src/app/t-to-b-slider-area/t-to-b-slider-area.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-t-to-b-slider-area',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TToBSliderAreaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private elementRef: ElementRef
+  ) { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $('.slider8').bxSlider({
+       mode: 'vertical',
+       slideWidth: 300,
+       minSlides: 3,
+       slideMargin: 10
+       });
+      $('.slider9').bxSlider({
+       mode: 'vertical',
+       slideWidth: 300,
+       minSlides: 3,
+       slideMargin: 10
+       });
+      $('.slider10').bxSlider({
+       mode: 'vertical',
+       slideWidth: 300,
+       minSlides: 3,
+       slideMargin: 10
+       });
+     });
   }
 
+  ngAfterViewInit() {
+    const jquery = document.createElement('script');
+    jquery.type = 'text/javascript';
+    jquery.src = 'assets/js/jquery.bxslider.min.js';
+    this.elementRef.nativeElement.appendChild(jquery);
+  }
 }
