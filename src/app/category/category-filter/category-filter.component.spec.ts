fe1c3f7078f74708a05b6f09a89a44440c26fa32
韓悅華 <vhanxtreme@gmail.com>
2018-10-24 20:37:47 +0800

    Migrated: Category,Contact,Product-Detail

---
+++ src/app/category/category-filter/category-filter.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CategoryFilterComponent } from './category-filter.component';
+
+describe('CategoryFilterComponent', () => {
+  let component: CategoryFilterComponent;
+  let fixture: ComponentFixture<CategoryFilterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CategoryFilterComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CategoryFilterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/category/category-general/category-general.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-general',
+  templateUrl: './category-general.component.html',
+  styleUrls: ['./category-general.component.css']
+})
+export class CategoryGeneralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
