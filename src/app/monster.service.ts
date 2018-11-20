1c512778ac0baab123c0c4c6d725238190f4632f
LJP-TW <accr94238@gmail.com>
2018-11-21 02:03:26 +0800

    建立3筆假資料

--- src/app/monster.service.ts
+++ src/app/monster.service.ts
@@ -6,10 +6,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MonsterService {
 
+  // 假資料
   tempMonsters = [
     {
       id: 1, // 根據全國圖鑑
-      imgLoc: 'assets/images/pokemon/id001.png', // 請改最後一個斜線後的內容就好，到時候請一併把圖檔跟json檔案交給我,圖片大小請盡量壓在 300*350以內
+      name: '妙蛙種子',
+      attribute: ['poison', 'grass'],
+      imgNum: 1,
       HP: 200,
       ATTACK: 150,
       DEFENSE: 40,
@@ -20,40 +23,41 @@ export class MonsterService {
       discount: 20,
       price: 24000,
       description: '超想要的拉~在台科大抓到的妙蛙種子，是不是很稀有R?', // 隨意能填寫盡量寫
-      createdAt: '2018-05-11T19:40:35.917Z', // 隨意，注意->有些月份沒有31天/每一天只有24小時/每小時只有60分鐘/每分鐘只有60秒
-      updatedAt: '2018-05-13T19:49:35.917Z', // 請幫我填寫日期+2天，要幫我注意一下符合日期邏輯.
+      createdAt: '2018-05-11T19:40:35.917Z' // 隨意，注意->有些月份沒有31天/每一天只有24小時/每小時只有60分鐘/每分鐘只有60秒
     },
     {
-      id: 2, // 根據全國圖鑑
-      imgLoc: 'assets/images/pokemon/id002.png', // 請改最後一個斜線後的內容就好，到時候請一併把圖檔跟json檔案交給我,圖片大小請盡量壓在 300*350以內
+      id: 143,
+      name: '卡比獸',
+      attributes: ['normal'],
+      imgNum: 1,
       HP: 200,
       ATTACK: 150,
       DEFENSE: 40,
       SP_ATTACK: 230,
       SP_DEFENSE: 76,
       SPEED: 30,
-      sold: 30,
-      discount: 20,
-      price: 24000,
-      description: '超想要的拉~在台科大抓到的妙蛙種子，是不是很稀有R?', // 隨意能填寫盡量寫
-      createdAt: '2018-05-11T19:40:35.917Z', // 隨意，注意->有些月份沒有31天/每一天只有24小時/每小時只有60分鐘/每分鐘只有60秒
-      updatedAt: '2018-05-13T19:49:35.917Z', // 請幫我填寫日期+2天，要幫我注意一下符合日期邏輯.
+      sold: 2,
+      discount: 0,
+      price: 87,
+      description: '超想要的拉~在台科大抓到的妙蛙種子，是不是很稀有R?',
+      createdAt: '2018-07-31T19:40:35.917Z'
     },
     {
-      id: 3, // 根據全國圖鑑
-      imgLoc: 'assets/images/pokemon/id003.png', // 請改最後一個斜線後的內容就好，到時候請一併把圖檔跟json檔案交給我,圖片大小請盡量壓在 300*350以內
+      id: 252,
+      name: '木守宮',
+      attributes: ['grass'],
+      imgNum: 1,
       HP: 200,
       ATTACK: 150,
       DEFENSE: 40,
       SP_ATTACK: 230,
       SP_DEFENSE: 76,
       SPEED: 30,
-      sold: 30,
-      discount: 20,
-      price: 24000,
-      description: '超想要的拉~在台科大抓到的妙蛙種子，是不是很稀有R?', // 隨意能填寫盡量寫
-      createdAt: '2018-05-11T19:40:35.917Z', // 隨意，注意->有些月份沒有31天/每一天只有24小時/每小時只有60分鐘/每分鐘只有60秒
-      updatedAt: '2018-05-13T19:49:35.917Z', // 請幫我填寫日期+2天，要幫我注意一下符合日期邏輯.
+      sold: 2,
+      discount: 10,
+      price: 90,
+      description: '資工系學會饒了我吧, 饒了我吧饒了我吧, 守宮守宮, 慈母手中線',
+      createdAt: '2018-03-12T15:30:22.123Z'
     }
   ];
 
