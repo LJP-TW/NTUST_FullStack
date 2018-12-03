4337da7df7f5509b397d71434cb63387049b34a2
LJP-TW <accr94238@gmail.com>
2018-12-04 03:26:41 +0800

    promotion-area 導至 category

--- src/app/promotion-area/promotion-area.component.ts
+++ src/app/promotion-area/promotion-area.component.ts
@@ -1,3 +1,4 @@
+import { CategoryService } from './../category.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromotionAreaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public categoryService: CategoryService) { }
 
   ngOnInit() {
   }
